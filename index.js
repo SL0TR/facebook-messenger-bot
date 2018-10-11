f055c5ece2d42d68a895c8cb59dce5c3353d4bf4
SL0TR <mohaiminislam94@gmail.com>
2018-10-11 17:27:18 +0600

    add procfile and update port  environtment

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 "use strict";
 
 // Imports dependencies and set up http server
-const express = require('express'),
-
-messengerWebbhookController = require('./controllers/messengerWebhook'),
-
-app = express();
-
+const express = require("express"),
+  messengerWebbhookController = require("./controllers/messengerWebhook"),
+  PORT = process.env.PORT || 1337,
+  app = express();
 
 // Fire controllers
 messengerWebbhookController(app);
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-
-
+app.listen(process.env.PORT || 1337, () =>
+  console.log(`Server is listening on port ${PORT}`)
+);
