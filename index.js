285ad3519f6f8cc23e98892817d0b5600f62ea36
SL0TR <mohaiminislam94@gmail.com>
2018-10-14 17:52:29 +0600

    revert back port

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 // Imports dependencies and set up http server
 const express = require("express"),
   messengerWebbhookController = require("./controllers/messengerWebhook"),
-  PORT = process.env.PORT || 8080,
+  PORT = process.env.PORT || 1337,
   app = express();
 
 // Fire controllers
 messengerWebbhookController(app);
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 8080, () =>
+app.listen(process.env.PORT || 1337, () =>
   console.log(`Server is listening on port ${PORT}`)
 );
