e01ea09d2989fc6e38c36aca070380f833ab3090
SL0TR <mohaiminislam94@gmail.com>
2018-10-22 17:21:50 +0600

    get back to previous port

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 // Imports dependencies and set up http server
 const express = require("express"),
   messengerWebbhookController = require("./controllers/messengerWebhook"),
-  PORT = process.env.PORT || 3000,
+  PORT = process.env.PORT || 1337,
   app = express();
 
 // Fire controllers
 messengerWebbhookController(app);
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`Server is running at https://bot.boomerangbd.com:${PORT}`)
+app.listen(process.env.PORT || 1337, () =>
+  console.log(`Server is listening on port ${PORT}`)
 );
