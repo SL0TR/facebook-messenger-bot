c47585e4c3db04f919353c074e79c760b25893f9
SL0TR <mohaiminislam94@gmail.com>
2018-10-22 18:24:17 +0600

    add simple langing page

--- bot.js
+++ bot.js
@@ -9,6 +9,19 @@ const express = require("express"),
 // Fire controllers
 messengerWebbhookController(app);
 
+// serve static files
+app.use(express.static('public'));
+
+// Send static site
+app.get('/', (req, res) => {
+  console.log('Request was made from: ' + req.url);
+  res.sendFile(__dirname + '/public/index.html', err => {
+    if (err) {
+      res.status(500).send.err
+    }
+  })
+})
+
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () =>
   console.log(`Server is listening on port ${PORT}`)
