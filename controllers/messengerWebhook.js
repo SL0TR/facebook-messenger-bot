c8254584046b910265bb422f4023673c3b71a2e0
SL0TR <mohaiminislam94@gmail.com>
2018-10-23 14:07:35 +0600

    handle message and send response via request ajax

--- controllers/messengerWebhook.js
+++ controllers/messengerWebhook.js
@@ -1,7 +1,9 @@
 "use strict";
 
 // Imports dependencies and set up http server
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"),
+  request = require('request');
+
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
@@ -25,6 +27,14 @@ module.exports = app => {
         // Get the sender PSID
         let sender_psid = webhook_event.sender.id;
         console.log('Sender PSID: ' + sender_psid);
+
+        // Check if the event is a message or postback and
+        // pass the event to the appropriate handler function
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message);        
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback);
+        }
       
       });
 
@@ -60,9 +70,25 @@ module.exports = app => {
     }
   });
 
+
+  // Handles messages events
   function handleMessage(sender_psid, received_message) {
 
+    let response;
+  
+    // Check if the message contains text
+    if (received_message.text) {    
+  
+      // Create the payload for a basic text message
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
+    }  
+    
+    // Sends the response message
+    callSendAPI(sender_psid, response);    
   }
+
   
   // Handles messaging_postbacks events
   function handlePostback(sender_psid, received_postback) {
@@ -71,7 +97,26 @@ module.exports = app => {
   
   // Sends response messages via the Send API
   function callSendAPI(sender_psid, response) {
-    
+    // Construct the message body
+    let request_body = {
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
+  
+    // Send the HTTP request to the Messenger Platform
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }); 
   }
-
 };
