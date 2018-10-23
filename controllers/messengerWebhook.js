61d338a08ff78d6e51ff7e48228acad631e1067a
SL0TR <mohaiminislam94@gmail.com>
2018-10-23 15:26:34 +0600

    add attachment and postback support

--- controllers/messengerWebhook.js
+++ controllers/messengerWebhook.js
@@ -73,28 +73,68 @@ module.exports = app => {
 
   // Handles messages events
   function handleMessage(sender_psid, received_message) {
-
     let response;
-  
-    // Check if the message contains text
-    if (received_message.text) {    
-  
-      // Create the payload for a basic text message
-      response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an image!`
-      }
-    }  
     
-    // Sends the response message
+    // Checks if the message contains text
+    if (received_message.text) {    
+      // Create the payload for a basic text message, which
+      // will be added to the body of our request to the Send API
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      }
+    } else if (received_message.attachments) {
+      // Get the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+      response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Le tek yo emej",
+              "subtitle": "Tap dat button to answer",
+              "image_url": attachment_url,
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Yes!",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "No!",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
+        }
+      }
+    } 
+    
+    // Send the response message
     callSendAPI(sender_psid, response);    
   }
 
   
   // Handles messaging_postbacks events
   function handlePostback(sender_psid, received_postback) {
+    let response;
+    
+    // Get the payload for the postback
+    let payload = received_postback.payload;
   
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+      response = { "text": "You're welcome :)" }
+    } else if (payload === 'no') {
+      response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
   }
-  
+
+
   // Sends response messages via the Send API
   function callSendAPI(sender_psid, response) {
     // Construct the message body
