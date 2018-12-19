8a6fa3cc2d0ddcc20528307043bd2a30708caaea
SL0TR <mohaiminislam94@gmail.com>
2018-12-19 12:36:04 +0000

    refactor code

--- server/services/services.js
+++ server/services/services.js
@@ -1,111 +1,128 @@
-exports.servicesBtns = {
-  tabOne : [
-    {
-      type: "postback",
-      title: "Marketing",
-      payload: "marketing"
-    },
-    {
-      type: "postback",
-      title: "Video Prduction",
-      payload: "vidProd"
-    },
-    {
-      type: "postback",
-      title: "Branding",
-      payload: "branding"
-    }
-  ],
-  tabTwo: [
-    {
-      type: "postback",
-      title: "Mobile App and Web Development",
-      payload: "mobWebDev"
-    },
-    {
-      type: "postback",
-      title: "UX / UI Design",
-      payload: "uxUi"
-    },
-    {
-      type: "postback",
-      title: "Content",
-      payload: "content"
-    }
-  ]
-}
+exports.servicesBtnList = [
+  {
+    title: "Marketing",
+    buttons: [
+      {
+        type: "postback",
+        title: "Marketing",
+        payload: "marketing"
+      },
+      {
+        type: "postback",
+        title: "Video Prduction",
+        payload: "vidProd"
+      },
+      {
+        type: "postback",
+        title: "Branding",
+        payload: "branding"
+      }
+    ]
+  },
+  {
+    title: "Tech",
+    buttons: [
+      {
+        type: "postback",
+        title: "Mobile App and Web Development",
+        payload: "mobWebDev"
+      },
+      {
+        type: "postback",
+        title: "UX / UI Design",
+        payload: "uxUi"
+      },
+      {
+        type: "postback",
+        title: "Content",
+        payload: "content"
+      }
+    ]
+  }
+];
 
+exports.portfolioBtnList = [
+  {
+    title: "Digital Marketing",
+    buttons: [
+      {
+        type: "postback",
+        title: "Digital Marketing",
+        payload: "marketing-portfolio"
+      },
+      {
+        type: "postback",
+        title: "Video Production",
+        payload: "vidProd-portolio"
+      },
+      {
+        type: "postback",
+        title: "Branding",
+        payload: "branding-portfolio"
+      }
+    ]
+  },
+  {
+    title: "Tech",
+    buttons: [
+      {
+        type: "postback",
+        title: "Mobile App and Web Development / Design",
+        payload: "mobWebDev-portfolio"
+      },
+      {
+        type: "postback",
+        title: "Case Studies",
+        payload: "case-portfolio"
+      },
+      {
+        type: "web_url",
+        url: "https://www.boomerangbd.com/our-work/all/",
+        title: "View All"
+      }
+    ]
+  }
+];
 
-exports.portfolioBtns = {
-  tabOne: [
-    {
-      type: "postback",
-      title: "Digital Marketing",
-      payload: "marketing-portfolio"
-    },
-    {
-      type: "postback",
-      title: "Video Production",
-      payload: "vidProd-portolio"
-    },
-    {
-      type: "postback",
-      title: "Branding",
-      payload: "branding-portfolio"
-    }
-  ],
-  tabTwo: [
-    {
-      type: "postback",
-      title: "Mobile App and Web Development / Design",
-      payload: "mobWebDev-portfolio"
-    },
-    {
-      type: "postback",
-      title: "Case Studies",
-      payload: "case-portfolio"
-    },
-    {
-      type: "web_url",
-      url: "https://www.boomerangbd.com/our-work/all/",
-      title: "View All"
-    }
-  ]
-}
-
-exports.boomerangBtns = {
-  tabOne: [
-    {
-      type: "postback",
-      title: "Our Services",
-      payload: "services"
-    },
-    {
-      type: "postback",
-      title: "Our Portfolio",
-      payload: "portfolio"
-    },
-    {
-      type: "postback",
-      title: "Our Clients",
-      payload: "clients"
-    }
-  ],
-  tabTwo: [
-    {
-      type: "postback",
-      title: "Available Jobs",
-      payload: "job"
-    },
-    {
-      type: "postback",
-      title: "Need Assistance?",
-      payload: "assistance"
-    },
-    {
-      type: "web_url",
-      url: "https://www.boomerangbd.com",
-      title: "Visit Our Site"
-    }
-  ]
-}
+exports.boomerangBtnList = [
+  {
+    title: "Click on the section you would like to know more about",
+    buttons: [
+      {
+        type: "postback",
+        title: "Our Services",
+        payload: "services"
+      },
+      {
+        type: "postback",
+        title: "Our Portfolio",
+        payload: "portfolio"
+      },
+      {
+        type: "postback",
+        title: "Our Clients",
+        payload: "clients"
+      }
+    ]
+  },
+  {
+    title: "Click on the section you would like to know more about",
+    buttons: [
+      {
+        type: "postback",
+        title: "Available Jobs",
+        payload: "job"
+      },
+      {
+        type: "postback",
+        title: "Need Assistance?",
+        payload: "assistance"
+      },
+      {
+        type: "web_url",
+        url: "https://www.boomerangbd.com",
+        title: "Visit Our Site"
+      }
+    ]
+  }
+];
